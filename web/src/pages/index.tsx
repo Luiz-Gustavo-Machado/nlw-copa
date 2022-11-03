f63536753d23b8b0593f56dccc98dbdb6ddb3f5b
Luiz Gustavo Machado <luiz.gustavo.machado@hotmail.com>
2022-11-03 17:16:58 -0300

    upload

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,14 +1,58 @@
-interface HomeProps {
-  count: number;
-}
+import Image from 'next/image'
+import appPreviewImg from '../assets/app-nlw-copa-preview.png'
+import logoImg from '../assets/logo.svg'
+import usersAvatarExampleImg from '../assets/users-avatar-example.png'
+import iconCheckImg from '../assets/icon-check.svg';
 
-export default function Home(props: HomeProps) {
+export default function Home() {
+  
   return (
-    <h1 className="text-violet-400 font-bold text-4xl">Contagem: {props.count} </h1>
+    <div className="max-w-[1124px] h-screen mx-auto grid-cols-2 intems-center">
+      <main>
+        <Image src={logoImg} alt='NLW Copa' />
+
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight"> 
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div>
+          <Image src={usersAvatarExampleImg} alt="" />
+          <strong>
+            <span>+12.592</span> pessoas já usando
+          </strong>
+        </div>
+
+        <form>
+          <input type="text" required placeholder='Qual nome do seu bolão?' />
+          <button type='submit'>Criar meu bolão</button>
+        </form>
+
+        <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+
+        <div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Image src={appPreviewImg} alt="Dois celulares exibindo uma prévia do bolão NLW copa"
+        quality={100} />
+    </div>
   )
 }
 
-export const getServerSideProps = async () => {
+/*export const getServerSideProps = async () => {
   const response = await fetch('http://127.0.0.1:3333/pools/count')
   const data = await response.json()
   
@@ -17,4 +61,4 @@ export const getServerSideProps = async () => {
       count: data.count,
     }
   }
-}
+}*/
