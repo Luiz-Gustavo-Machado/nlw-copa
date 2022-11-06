22cd0b4751cd8e962d3a338751f5a962c7022c6c
Luiz Gustavo Machado <luiz.gustavo.machado@hotmail.com>
2022-11-06 20:42:28 -0300

    ukp

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -7,14 +7,14 @@ import { api } from '../services/api';
 
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
-import { PoolCard, PoolCardPros } from '../components/PoolCard';
+import { PoolCard, PoolCardProps } from '../components/PoolCard';
 import { Loading } from '../components/Loading';
 import { EmptyPoolList } from '../components/EmptyPoolList';
 
 
 export function Pools() {
   const [isLoading, setIsLoading] = useState(true);
-  const [pools, setPools] = useState<PoolCardPros[]>([])
+  const [pools, setPools] = useState<PoolCardProps[]>([])
   
   const { navigate } = useNavigation();
   const toast = useToast();
@@ -42,7 +42,7 @@ export function Pools() {
 
   return (
     <VStack flex={1} bgColor="gray.900">
-      <Header title="MEUS BOLÕES" />
+      <Header title="MEUS BOLÕES"/>
 
       <VStack mt={6} mx={5} borderBottomWidth={1} borderBottomColor="gray.600" pb={4} mb={4}>
         <Button 
